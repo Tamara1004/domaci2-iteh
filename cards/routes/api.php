fb1c8d2769297c95cb003b80a323b40712170c82
Tamara Calic <tamara.calic1004@gmail.com>
2023-02-12 03:39:09 +0100

    Added Controllers

--- cards/routes/api.php
+++ cards/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('register',[AuthController::class,'register']);
+Route::post('login',[AuthController::class,'login']);
+
+Route::apiResource('cards',CardController::class)->only(['index','show']);
+Route::middleware('auth:sanctum')->group(function(){
+
+    Route::post('logout',[AuthController::class,'logout']);
+    Route::apiResource('cards',CardController::class)->except(['index','show']);
 });
